c95a1dcdd620b2868adca96d5ebc3cd7c4b11922
nadiiahonchar1 <nadiiahonchar1@gmail.com>
2024-10-25 18:27:09 +0300

    add page params to url

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useSearchParams,
+} from 'react-router-dom';
 import { fetchHeroes } from './api/api';
 import HeroItem from './components/HeroItem/HeroItem';
 import HeroDetail from './components/HeroDetail/HeroDetail';
@@ -7,11 +12,13 @@ import styles from './App.module.css';
 
 const App = () => {
   const [heroes, setHeroes] = useState<HeroType[]>([]);
-  const [page, setPage] = useState(1);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
   const [nextPage, setNextPage] = useState<string | null>(null);
   const [prevPage, setPrevPage] = useState<string | null>(null);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = Number(searchParams.get('page')) || 1;
 
   // useEffect to fetch heroes whenever the page number changes
   useEffect(() => {
@@ -34,7 +41,8 @@ const App = () => {
 
   // Function to change the page (increment or decrement based on direction)
   const changePage = (direction: number) => {
-    setPage((prev) => Math.max(prev + direction, 1));
+    const newPage = Math.max(page + direction, 1);
+    setSearchParams({ page: newPage.toString() });
   };
 
   // Display loading state
@@ -44,32 +52,30 @@ const App = () => {
   if (error) return <div>{error}</div>;
 
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className={styles.container}>
-              <h1>List of Star Wars heroes</h1>
-              <ul className={styles.list}>
-                {heroes.map((hero) => (
-                  <HeroItem key={hero.id} id={hero.id} name={hero.name} />
-                ))}
-              </ul>
-              <div>
-                <button onClick={() => changePage(-1)} disabled={!prevPage}>
-                  Previous
-                </button>
-                <button onClick={() => changePage(1)} disabled={!nextPage}>
-                  Next
-                </button>
-              </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className={styles.container}>
+            <h1>List of Star Wars heroes</h1>
+            <ul className={styles.list}>
+              {heroes.map((hero) => (
+                <HeroItem key={hero.id} id={hero.id} name={hero.name} />
+              ))}
+            </ul>
+            <div>
+              <button onClick={() => changePage(-1)} disabled={!prevPage}>
+                Previous
+              </button>
+              <button onClick={() => changePage(1)} disabled={!nextPage}>
+                Next
+              </button>
             </div>
-          }
-        />
-        <Route path="/hero/:id" element={<HeroDetail />} />
-      </Routes>
-    </Router>
+          </div>
+        }
+      />
+      <Route path="/hero/:id" element={<HeroDetail />} />
+    </Routes>
   );
 };
 
--- src/components/HeroItem/HeroItem.tsx
+++ src/components/HeroItem/HeroItem.tsx
@@ -7,7 +7,7 @@ const HeroItem: React.FC<HeroItemProps> = ({ id, name }) => {
   return (
     // Render a list item representing a character card
     <li className={styles.characterCard}>
-      <Link to={`/hero/${id}`} ria-label={`View details about ${name}`}>
+      <Link to={`/hero/${id}`} aria-label={`View details about ${name}`}>
         <img
           src={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
           alt={name}
